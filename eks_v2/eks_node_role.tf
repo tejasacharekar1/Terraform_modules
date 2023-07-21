03ab393f2ff8e891f352f9605a65ef7e95bdb4a9
Tejas Acharekar <103490327+tejasacharekar1@users.noreply.github.com>
2023-07-21 10:50:59 +0530

    Added Autoscalling Policy

--- eks_v2/eks_node_role.tf
+++ eks_v2/eks_node_role.tf
@@ -31,3 +31,33 @@ resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOn
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.node.name
 }
+
+resource "aws_iam_policy" "Autoscaller_worker_node" {
+  name        = "eks_autoscaller"
+  description = "Policy for EKS Autoscaller"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "autoscaling:DescribeAutoScalingGroups",
+            "autoscaling:DescribeAutoScalingInstances",
+            "autoscaling:DescribeLaunchConfigurations",
+            "autoscaling:DescribeTags",
+            "autoscaling:SetDesiredCapacity",
+            "autoscaling:TerminateInstanceInAutoScalingGroup",
+            "ec2:DescribeLaunchTemplateVersions"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_policy_attachment" "worker_node_autoscaller" {
+  policy_arn = aws_iam_policy.Autoscaller_worker_node.arn
+  roles      = aws_iam_role.node
+}

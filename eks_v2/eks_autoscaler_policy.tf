3a34cbdcd490ed0a920dbd31509569ccbb191fe1
Tejas Acharekar <103490327+tejasacharekar1@users.noreply.github.com>
2023-08-01 14:40:54 +0530

    Update eks_autoscaler_policy

--- eks_v2/eks_autoscaler_policy.tf
+++ eks_v2/eks_autoscaler_policy.tf
@@ -1,7 +1,6 @@
-resource "aws_iam_role_policy" "Autoscaller_worker_node" {
+resource "aws_iam_policy" "Autoscaller_worker_node" {
   name        = "eks_autoscaller"
   description = "Policy for EKS Autoscaller"
-  role        = aws_iam_role.worker_node
   policy = jsonencode(
     {
       "Version" : "2012-10-17",

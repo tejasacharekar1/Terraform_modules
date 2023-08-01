a50ab5461f48b278f2fb0028c0ddaacca1306d20
Tejas Acharekar <103490327+tejasacharekar1@users.noreply.github.com>
2023-08-01 14:58:11 +0530

    Update role arn

--- eks_v2/eks_node_role.tf
+++ eks_v2/eks_node_role.tf
@@ -19,17 +19,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.worker_node.name
 }
 
 resource "aws_iam_role_policy_attachment" "node_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.worker_node.name
 }
 
 resource "aws_iam_role_policy_attachment" "node_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.node.name
+  role       = aws_iam_role.worker_node.name
 }
 
 resource "aws_iam_role_policy_attachment" "Eks-policy-attchment" {

76548c3f1b0f958b9f42991b0a56be84d4d0bd9b
Tejas Acharekar <103490327+tejasacharekar1@users.noreply.github.com>
2023-08-01 14:58:51 +0530

    update role arn

--- eks_v2/eks_node_pub_gp.tf
+++ eks_v2/eks_node_pub_gp.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "tj_eks_node_pub_group" {
   cluster_name    = aws_eks_cluster.tj-eks-cluster.name
   node_group_name = "${var.project}-pub-ng"
-  node_role_arn   = aws_iam_role.node.arn
+  node_role_arn   = aws_iam_role.worker_node.arn
   subnet_ids      = var.public_subnet_ids
 
   scaling_config {
